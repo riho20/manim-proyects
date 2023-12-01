c6fc9b8d7ae65d9e12944886116c2cb45914731e
riho20 <152660036+riho20@users.noreply.github.com>
2023-12-01 23:24:20 +0100

    atractor de lorenz.py

--- atractor de lorenz.py
+++ atractor de lorenz.py
@@ -1,40 +1,44 @@
-from manim import *
-import math 
-import  random
-class sferita(ThreeDScene): 
+rom manim import *
+
+class Lorenz_Attractorlargo(ThreeDScene):
     def construct(self):
         
+        dot = Sphere(radius=0.05,fill_color=BLUE).move_to(0*RIGHT + 0.1*UP + 0.105*OUT)
         
-        
-        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.begin_ambient_camera_rotation(rate=0.4)
-        x = 2
-        y = 1
-        z = 1
-        σ = 10
-        ρ = 28
-        β = 8/3
-        for i in range(100):
-            
-            x1= 0.1 * (y-x)
-            y1 = x*(0.28-z)-y
-            z1 = x*y - (0.08/3)*z
-            dx = σ * (y - x)         
-            x += dx/10000
-            dy = x * (ρ - z) - y
-            y += dy/10000
-            dz = x * y - β * z
-            z +=  dz/10000
-            
-            
-            self.add(Dot([x,y,z],0.05))
-            x=x1
-            y=y1
-            z=z1
-        self.wait(5)
+        self.set_camera_orientation(phi=65 * DEGREES,theta=30*DEGREES,gamma = 90*DEGREES)  
+        self.begin_ambient_camera_rotation(rate=0.05)            #Start move camera
 
+        dtime = 0.01
+        numsteps = 30
 
-           
+        self.add(dot)
+
+        def lorenz(x, y, z, s=10, r=28, b=2.667):
+            x_dot = s*(y - x)
+            y_dot = r*x - y - x*z
+            z_dot = x*y - b*z
+            return x_dot, y_dot, z_dot
+
+        def update_trajectory(self, dt):
+            new_point = dot.get_center()
+            if np.linalg.norm(new_point - self.points[-1]) > 0.01:
+                self.add_smooth_curve_to(new_point)
+#(new_point - self.points[-1])/np.linalg.norm(new_point - self.points[-1]) > 0.01
+        traj = VMobject()
+        traj.start_new_path(dot.get_center())
+        traj.set_stroke(BLUE, 1.5, opacity=0.8)
+        traj.add_updater(update_trajectory)
+        self.add(traj)
+
+        def update_position(self,dt):
+            x_dot, y_dot, z_dot = lorenz(dot.get_center()[0]*10, dot.get_center()[1]*10, dot.get_center()[2]*10)
+            x = x_dot * dt/10
+            y = y_dot * dt/10
+            z = z_dot * dt/10
+            self.shift(x/10*RIGHT + y/10*UP + z/10*OUT)
+
+        dot.add_updater(update_position)
+        self.wait(240*2)
           
        
        
